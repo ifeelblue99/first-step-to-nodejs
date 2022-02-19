74e0dc14418aba99e135aedae5921167469d2745
Baran Cakirbey <barancakirbey13@gmail.com>
2022-02-19 16:57:43 +0300

    about.html created

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const http = require("http")
+const fs = require("fs")
 
 const server = http.createServer((req, res)=>{
     console.log("request made", req.url, req.method)
 
+    // set header
     res.setHeader("Content-Type", "text/html")
-    res.write("<a href='https://github.com/ifeelblue99'>click me!</a>")
-    res.end()
+    
+    // send an html file
+    fs.readFile("./views/about.html", (err, data)=>{
+        if (err) {
+            console.log(err)
+        }else{
+            res.write(data)
+            res.end()
+        }
+    })
+
 })
 
 server.listen(3000, "localhost", ()=>{
     console.log("listening")
-
 })
